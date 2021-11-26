a597c9c11eec5cd8fab6609cf91c8d1edf3cada1
Polyane Tuag <pollyportugues@hotmail.com>
2021-11-26 19:27:02 -0300

    adding stat routes

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -54,4 +54,10 @@ module.exports = (app) => {
   app.post("/signup", app.api.user.save);
   app.post("/signin", app.api.auth.signin);
   app.post("/validateToken", app.api.auth.validateToken);
+
+  // (Tela) Estatísticas sobre a aplicação
+  app
+    .route("/stats")
+    .all(app.config.passport.authenticate())
+    .get(app.api.stat.get);
 };

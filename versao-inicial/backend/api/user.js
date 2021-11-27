540e19ae9544414e412da53bd76b16d46b9e8b67
Polyane Tuag <pollyportugues@hotmail.com>
2021-11-27 10:15:34 -0300

    validate administrator registration

--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -12,6 +12,11 @@ module.exports = (app) => {
     const user = { ...res.body };
     if (req.params.id) user, (id = req.params.id);
 
+    //validando usuário admin
+    if (!req.originalUrl.startsWith("/users")) user.admin = false;
+
+    if (!req.user || !req.user.admin) user.admin = false;
+
     try {
       existsOrError(user.name, "Nome não informado");
       existsOrError(user.email, "Email não informado");

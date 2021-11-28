bf1bc98c0b7dc9e15eae655be907cb550b0fbe89
Polyane Tuag <pollyportugues@hotmail.com>
2021-11-28 10:36:04 -0300

    rendering components

--- versao-inicial/frontend/src/App.vue
+++ versao-inicial/frontend/src/App.vue
@@ -1,15 +1,44 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Content />
+		<Header />
+		<Menu />
+		<Footer />
 	</div>
 </template>
 
 <script>
+import Content from './components/template/Content'
+import Header from './components/template/Header'
+import Menu from './components/template/Menu'
+import Footer from './components/template/Footer'
+
 export default {
 	name: "App",
+	components: { Content, Header, Menu, Footer}
 }
 </script>
 
 <style>
+	* {
+		font-family: 'Lato, sans-serif';
+	}
 
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas:
+			'header header'
+			'menu content'
+			'menu footer' ;
+
+	}
 </style>

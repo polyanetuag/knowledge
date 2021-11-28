617d3c05943066e5961ee5a49782ecc25763ef15
Polyane Tuag <pollyportugues@hotmail.com>
2021-11-28 10:35:44 -0300

    creating footer component

---
+++ versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong>Cod3r</strong> © 2015-2021</span>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
+
+<style>
+  .footer {
+    grid-area: footer;
+    background: #DDD;
+    color: #333;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-right: 10px;
+  }
+</style>

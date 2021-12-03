78389aa395db092896aa8075b87237dfb060807a
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-03 20:27:18 -0300

    importing msgs file

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -2,6 +2,7 @@ import "font-awesome/css/font-awesome.css";
 import Vue from "vue";
 import App from "./App";
 import "./config/bootstrap";
+import "./config/msgs";
 import store from "./config/store";
 import router from "./config/router";
 

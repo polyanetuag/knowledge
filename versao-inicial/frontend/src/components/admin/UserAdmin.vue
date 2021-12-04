c04afab668d505667f216384ff0027d798cf069f
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-04 11:23:52 -0300

    creating registration form for the users tab

--- versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="user-admin">
+    <b-form>
+      <input id="user-id" type="hidden" v-model="user.id">
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Nome:" label-for="user-name">
+            <b-form-input id="user-name" type="text" v-model="user.name" required placeholder="Informe o nome do usuário..." />
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group label="E-mail:" label-for="user-email">
+            <b-form-input id="user-email" type="text" v-model="user.email" required placeholder="Informe o e-mail do usuário..." />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">
+        Administrador?
+      </b-form-checkbox>
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Senha:" label-for="user-password">
+            <b-form-input id="user-password" type="password" v-model="user.password" required placeholder="Informe a senha do usuário..." />
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group label="Confirmação de senha:" label-for="user-confirm-password">
+            <b-form-input id="user-confirm-password" type="password" v-model="user.confirmPassword" required placeholder="Confirme a senha do usuário..." />
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </b-form>
     <b-table hover striped :items='users' :fields="fields"></b-table>
   </div>
 </template>

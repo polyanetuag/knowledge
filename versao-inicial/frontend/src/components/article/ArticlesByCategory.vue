60b4f87948893c86c04878d777b2695908b75b11
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-08 07:44:18 -0300

    importing article listing page

--- versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
+++ versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
@@ -3,7 +3,7 @@
     <PageTitle icon='fa fa-folder-o' :main='category.name' sub='Categoria'/>
     <ul>
       <li v-for="article in articles" :key="article.id">
-        {{ article.name }}
+        <ArticleItem :article="article"/>
       </li>
     </ul>
     <div class="load-more">
@@ -16,10 +16,11 @@
 import { baseApiUrl } from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle.vue'
+import ArticleItem from './ArticleItem'
 
 export default {
   name: 'ArticlesByCategory',
-  components: { PageTitle },
+  components: { PageTitle, ArticleItem },
   data: function() {
     return {
       category: {},

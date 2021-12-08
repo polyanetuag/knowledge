5d22d5cceb0779a9d2d0c19f73e2749aba3ea208
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-08 08:46:03 -0300

    navigation between menus

--- versao-inicial/frontend/src/components/template/Menu.vue
+++ versao-inicial/frontend/src/components/template/Menu.vue
@@ -32,7 +32,16 @@ import axios from 'axios'
       getTreeData() {
         const url = `${baseApiUrl}/categories/tree`
         return axios.get(url).then(res => res.data)
+      },
+      onNodeSelect(node) {
+        this.$router.push({
+          name: 'articlesByCategory',
+          params: { id: node.id}
+        })
       }
+    },
+    mounted() {
+      this.$refs.tree.$on('node:selected', this.onNodeSelect)
     }
   }
 </script>
